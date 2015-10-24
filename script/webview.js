3ae9c7d4611e2f6d774c0c03066f4ec2931b5db7
Chris Root <hammer65@gmail.com>
2015-10-24 17:42:27 -0500

    browser class cleanup

--- script/webview.js
+++ script/webview.js
@@ -25,7 +25,6 @@ function WebViewWrapper(onload) {
         
         This.document = wrap(This.engine.executeScript("document"));
         This.window = wrap(This.engine.executeScript("window"));
-        //This.window.executeScript("window").setMember('HOST',new testObj());
         setLocationListener();
 
         // Call users onload function.
@@ -59,57 +58,17 @@ function WebViewWrapper(onload) {
   // Fire the ARMSG event in the window when we want to execute something in the container
   This.containerExec = function(msg){
     This.engine.executeScript(
-      "var AREVENT = new CustomEvent('ARMSG',{detail: '${msg}'});\n" + 
-      "document.dispatchEvent(AREVENT);"
+      "var EVENT = new CustomEvent('${This.customEventName}',{detail: '${msg}'});\n" + 
+      "document.dispatchEvent(EVENT);"
     );
   }
 
-  This.hostExec = function(msg){
-    This.emit("ARMSG",msg);
-  }
-
   function setLocationListener(){
-    // listen for location change. check URL before letting it load new location
-    // if it's an Applied arm (Applied Rater Message) URL we want it conrol the app not go somewhere
-    This.engine.locationProperty().addListener(new ChangeListener(){
+      This.engine.locationProperty().addListener(new ChangeListener(){
           changed: function(value, oldloc, newloc){
-            var load_URL = newloc;
-            var payload = {};
-            var URL_obj;
-            var method;
-            if(newloc.match('//appliedsystems.com/arm')){
-              load_URL = oldloc;
-              // Send out the event so we can execute the command
-              This.hostExec(processURL(newloc));
-            }
-            // JVM hates this if just loaded. thread it
-            Platform.runLater(function(){
-              This.load(load_URL)
-            });
+            
           }
       });
-  }
-
-  function parseQuery(qstr){
-    var out = {};
-    if(qstr){
-      var qarr = qstr.split('&');
-      var split;
-      for(var i = 0;i < qarr.length;i++){
-        split = qarr[i].split("=");
-        out[split[0]] = decoder(split[1],"UTF-8");
-      }
-    }
-    return out;
-  }
-
-  function processURL(strURL){
-    var payload = {};
-    var URL_obj = new URL(strURL);
-    method = URL_obj.path.match(/\/arm\/(\w+)?/);
-    payload = parseQuery(URL_obj.query);
-    payload.method = method? method[1] : "defaultMethod";
-    return payload;
   }
 }
 
